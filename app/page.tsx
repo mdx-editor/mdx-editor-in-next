314ad6a8a175e7fd3ffebf76d2a548937dd54489
Petyo Ivanov <underlog@gmail.com>
2024-01-04 10:47:58 +0200

    Upgrade to latest next/mdxeditor

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,20 @@ import { Suspense } from 'react'
 const EditorComp = dynamic(() => import('./EditorComponent'), { ssr: false })
 
 const markdown = `
-# Hello world!
-Check the EditorComponent.tsx file for the code .
+Hello **world**!
 `
+
 export default function Home() {
   return (
-    <div>Editor:
+    <>
+    <p>This is a bare-bones unstyled MDX editor without any plugins and no toolbar. Check the EditorComponent.tsx file for the code.</p>
+      <p>To enable more features, add the respective plugins to your instance - see <a className="text-blue-600" href="https://mdxeditor.dev/editor/docs/getting-started">the docs</a> for more details.</p>
+      <br />
+    <div style={{border: '1px solid black'}}>
       <Suspense fallback={null}>
         <EditorComp markdown={markdown} />
       </Suspense>
     </div>
+    </>
   )
 }
